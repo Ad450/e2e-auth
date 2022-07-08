a2408acf8c02b5ece3f0fb67e0588f50d3c34089
Ad450 <adjeimanue@gmail.com>
2022-07-08 13:29:51 +0000

    params added

--- src/Repository/todo_repository.ts
+++ src/Repository/todo_repository.ts
@@ -1,5 +1,5 @@
 abstract class TodoRepository {
     abstract createTodo(id: number, title: string, isChecked: boolean, subTitle?: string): Promise<Todo | null>;
-    abstract updateTodo(): Promise<Todo | null>;
+    abstract updateTodo(id: number, title: string, isChecked: boolean, subTitle?: string): Promise<Todo | null>;
     abstract deleteTodo(): Promise<Todo | null>;
 }
