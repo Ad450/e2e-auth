e2bcfcc41d241eec2f933bdf25ca7a31baf44e56
Ad450 <adjeimanue@gmail.com>
2022-07-09 00:47:43 +0000

    testing createTodo with handler

--- src/Repository/todo_respository_impl.ts
+++ src/Repository/todo_respository_impl.ts
@@ -1,17 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import { ParamsDictionary } from "express-serve-static-core";
-import { ParsedQs } from "qs";
+
 import { TodoRepository } from "./todo_repository";
+import { createTodoHandler } from "../middlewares/handle_response";
 
 export default class TodoRespositoryImpl extends TodoRepository {
-    createTodo(req: Request, res: Response, next: NextFunction): Promise<Todo | null> {
+    createTodo(req: Request, res: Response, next: NextFunction): void {
+        try {
+            createTodoHandler(req, res, next);
+        } catch (error) {
+            throw new Error((error as string).toString());
+        }
+    }
+    updateTodo(req: Request, res: Response, next: NextFunction): void {
         throw new Error("Method not implemented.");
     }
-    updateTodo(req: Request, res: Response, next: NextFunction): Promise<Todo | null> {
+    deleteTodo(req: Request, res: Response, next: NextFunction): void {
         throw new Error("Method not implemented.");
     }
-    deleteTodo(req: Request, res: Response, next: NextFunction): Promise<Todo | null> {
-        throw new Error("Method not implemented.");
-    }
+
 
 }
