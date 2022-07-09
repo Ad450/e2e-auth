fdf49cfebfa2365c79f995ea9b33f9430c05c7c1
Ad450 <adjeimanue@gmail.com>
2022-07-09 00:48:02 +0000

    refactored generate id

--- src/middlewares/generate_id.ts
+++ src/middlewares/generate_id.ts
@@ -1,16 +1,15 @@
 import { Response, Request, NextFunction } from "express";
 import hash from "object-hash";
-export const generateId = (req: Request, res: Response, next: NextFunction, isCreateTodo: boolean) => {
+
+export const generateId = (title: string, isChecked: boolean, isCreateTodo: boolean) => {
     if (isCreateTodo) {
         const hashValue = {
-            "title": req.body.title,
-            "isChecked": req.body.isChecked,
+            "title": title,
+            "isChecked": isChecked,
 
         }
 
         const id: string = hash(hashValue);
-        req.body.id = id;
+        return id;
     }
-
-    next();
 }
