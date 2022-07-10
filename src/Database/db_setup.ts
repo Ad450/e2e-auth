5f722f72e3323f949d86d24c6d233c8ac75c3455
Ad450 <adjeimanue@gmail.com>
2022-07-10 14:19:31 +0000

    minor changes

--- src/Database/db_setup.ts
+++ src/Database/db_setup.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-// placed here for testing
+// placed here for development purposes
 const mongooseUrl: string = "mongodb+srv://Emmanuel1999:Manu450666@cluster0.ynk8r7l.mongodb.net/test";
 
 const mongooseSetup = () => {
