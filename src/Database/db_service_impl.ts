4fd950129307e757260eb939a88ff037ab0d483b
Ad450 <adjeimanue@gmail.com>
2022-07-10 18:39:02 +0000

    update todo fixed

--- src/Database/db_service_impl.ts
+++ src/Database/db_service_impl.ts
@@ -6,6 +6,12 @@ export default class DatabaseServiceImpl extends DatabaseService {
     async create(data: DatabaseParam): Promise<void> {
         try {
             const { _title, _subTitle, _isChecked, _hash } = data;
+            // check if todo already exists
+            const retrieveTodo = Todo.findOne({ hash: _hash });
+            // todo already exists
+            if (retrieveTodo !== null || retrieveTodo !== undefined) {
+                return;
+            }
             const newTodo = new Todo({ title: _title, subTitle: _subTitle, isChecked: _isChecked, hash: _hash });
             await newTodo.save();
         } catch (error) {
@@ -13,20 +19,23 @@ export default class DatabaseServiceImpl extends DatabaseService {
             console.log(error);
         }
     }
+
+
     async update(data: DatabaseParam): Promise<void> {
         try {
             const { _isChecked, _subTitle, _title, _hash } = data;
-            const toBeUpdatedTodo = Todo.findById({ hash: _hash })
-            toBeUpdatedTodo.update({ title: _title, subTitle: _subTitle, isChecked: _isChecked, hash: _hash });
+            await Todo.updateOne({ hash: _hash, }, { title: _title, subTitle: _subTitle, isChecked: _isChecked, hash: _hash });
         } catch (error) {
             // will do proper error handling
             console.log(error);
         }
     }
+
+
     async delete(data: DatabaseParam): Promise<void> {
         try {
             const { _hash } = data;
-            Todo.remove({ hash: _hash });
+            await Todo.deleteOne({ hash: _hash });
         } catch (error) {
             console.log(error);
         }
