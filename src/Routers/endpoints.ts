4b325238c6cb4874563ea4ff61c066c00d968a4e
Ad450 <adjeimanue@gmail.com>
2022-07-10 13:46:47 +0000

    moved manual injection to injector fiile

--- src/Routers/endpoints.ts
+++ src/Routers/endpoints.ts
@@ -1,17 +1,7 @@
 
 import { IEndpoints, methods } from "./endpoints_interface";
-import { CreateTodo, } from "../Usecases/create_todo";
-import { UpdateTodo } from "../Usecases/update_todo";
-import { DeleteTodo } from "../Usecases/delete_todo";
-import TodoRespositoryImpl from "../Repository/todo_respository_impl";
-import EndpointsService from "../Services/endpoints_service";
+import Injector from "../di/app_di";
 
-
-
-// manually inject dependencies
-const createTodo = new CreateTodo(new TodoRespositoryImpl(new EndpointsService()));
-const updateTodo = new UpdateTodo(new TodoRespositoryImpl(new EndpointsService()));
-const deleteTodo = new DeleteTodo(new TodoRespositoryImpl(new EndpointsService()));
 
 const createTodoRoute: string = "/createTodo";
 const deleteTodoRoute: string = "/deleteTodo";
@@ -21,16 +11,16 @@ export const routes: Array<IEndpoints> = [
     {
         route: createTodoRoute,
         method: methods.POST,
-        handler: (req, res, next) => createTodo.execute(req, res, next)
+        handler: (req, res, next) => Injector.createTodo.execute(req, res, next)
     },
     {
         route: deleteTodoRoute,
         method: methods.POST,
-        handler: (req, res, next) => deleteTodo.execute(req, res, next),
+        handler: (req, res, next) => Injector.deleteTodo.execute(req, res, next),
     },
     {
         route: updateTodoRoute,
         method: methods.POST,
-        handler: (req, res, next) => updateTodo.execute(req, res, next)
+        handler: (req, res, next) => Injector.updateTodo.execute(req, res, next)
     }
 ]
